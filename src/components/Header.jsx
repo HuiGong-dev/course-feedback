f735c16b0c3a573ab604460bba7b7d34fa2398ea
HuiGong-dev <hellohuigong@gmail.com>
2022-06-06 19:21:18 +0200

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function Header({text, bgColor, textColor}) {
+    const headerStyle = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+
+    return (
+        <header style={headerStyle}>
+            <div className='container'>
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    text: 'Course Feedback',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#5cfe50'
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+}
+
+export default Header;
